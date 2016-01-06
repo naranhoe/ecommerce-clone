c044e53847c1d85b69422678f73855a139a05373
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-06 11:28:54 -0500

    Add sub-directory under Men

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <title>Naranhoe's Boutique</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/main.css">
-    <meta name="viewport" content="width=device-width, inital-scale=1, user-scalable=no">
-    <script src="https://ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-default navbar-fixed-top">
@@ -16,6 +16,12 @@
         <ul class="nav navbar-nav">
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Shirts</a></li>
+              <li><a href="#">Pants</a></li>
+              <li><a href="#">Shoes</a></li>
+              <li><a href="#">Accessories</a></li>
+            </ul>
           </li>
         </ul>
       </div>
