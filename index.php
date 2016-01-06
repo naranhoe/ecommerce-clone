fbef7173b9b51e37a24b33ce637f14a399d00091
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-06 11:06:03 -0500

    Add bootstrap and jquery functionality

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Naranhoe's Boutique</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <meta name="viewport" content="width=device-width, inital-scale=1, user-scalable=no">
+    <script src="https://ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
