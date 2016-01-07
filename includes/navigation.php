6546e4d39d026d34258d1f3c3a2d02f2c781625c
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-07 13:45:41 -0500

    Menu bar now renders live data from database

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,15 +8,19 @@
   <div class="container">
     <a href="index.php" class="navbar-brand">Naranhoe's Boutique</a>
     <ul class="nav navbar-nav">
-      <?php while($parent = mysqli_fetch_assoc($pquery)) :  ?>
-      <!-- Mens Cloths-->
+      <?php while($parent = mysqli_fetch_assoc($pquery)) : ?>
+        <?php
+          $parent_id = $parent['id'];
+          $sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
+          $cquery = $db->query($sql2);
+        ?>
+      <!-- Menu Items -->
       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
         <ul class="dropdown-menu" role="menu">
-          <li><a href="#">Shirts</a></li>
-          <li><a href="#">Pants</a></li>
-          <li><a href="#">Shoes</a></li>
-          <li><a href="#">Accessories</a></li>
+          <?php while($child = mysqli_fetch_assoc($cquery)) : ?>
+          <li><a href="#"><?php echo $child['category']?></a></li>
+          <?php endwhile; ?>
         </ul>
       </li>
     <?php endwhile; ?>
