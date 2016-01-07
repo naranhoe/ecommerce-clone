f4fc131f341d65ef8f5e1f990dd30f770b3872d2
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-07 11:55:10 -0500

    Clean up code

---
+++ includes/navigation.php
@@ -0,0 +1,60 @@
+<!-- Top Nav Bar-->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="index.php" class="navbar-brand">Naranhoe's Boutique</a>
+
+    <!-- Mens Cloths-->
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Womens Cloths -->
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Boys Cloths -->
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Boys<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Girls Cloths -->
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Girls<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+
+
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Naranhoe's Boutique</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/main.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-    <!-- Top Nav Bar-->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <a href="index.php" class="navbar-brand">Naranhoe's Boutique</a>
-        <ul class="nav navbar-nav">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Shirts</a></li>
-              <li><a href="#">Pants</a></li>
-              <li><a href="#">Shoes</a></li>
-              <li><a href="#">Accessories</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<?php
+  include 'includes/head.php';
+  include 'includes/navigation.php';
+?>
 
     <!-- Header -->
     <div id="headerWrapper">
