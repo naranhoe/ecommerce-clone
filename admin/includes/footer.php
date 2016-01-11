c48e6a08ba2968b66febaa1ad6f0659ed815a20f
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-11 16:36:19 -0500

    Add admin page

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+</div>
+<footer class="col-md-12 text-center" id="footer">&copy; Copyright 2015-2016 Naranhoe's Boutique</footer>
+
+</body>
+</html>
---
+++ admin/includes/navigation.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="index.php" class="navbar-brand">Naranhoe's Boutique Admin</a>
+    <ul class="nav navbar-nav">
+      <!-- Menu Items -->
+      <li><a href="brands.php">Brands</a></li>
+      <!-- <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#"></a></li>
+        </ul>
+      </li> -->
+    </ul>
+  </div>
+</nav>
