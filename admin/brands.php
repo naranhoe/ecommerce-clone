c7ba53d4509527b3e07adfefe54fe3227617baf8
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-11 17:06:34 -0500

    Brand now render dynamically inside admin page

---
+++ admin/brands.php
@@ -0,0 +1,24 @@
+<?php
+  require_once '../core/init.php';
+  include 'includes/head.php';
+  include 'includes/navigation.php';
+  // Get brands from database
+  $sql = "SELECT * FROM brand ORDER BY brand";
+  $results = $db->query($sql);
+?>
+<h2 class="text-center">Brands</h2>
+<table class="table table-bordered table-striped table-auto">
+  <thead>
+    <th></th><th>Brand</th><th></th>
+  </thead>
+  <tbody>
+    <?php while($brand = mysqli_fetch_assoc($results)) : ?>
+      <tr>
+        <td><a href="brands.php?edit=1" class='btn btn-xs btn-default'><span class="glyphicon glyphicon-pencil"></span></a></td>
+        <td><?php echo $brand['brand']; ?></td>
+        <td><a href="brands.php?delete=1" class='btn btn-xs btn-default'><span class="glyphicon glyphicon-remove-sign"></span></a></td>
+      </tr>
+  <?php endwhile; ?>
+  </tbody>
+</table>
+<?php include 'includes/footer.php'; ?>
