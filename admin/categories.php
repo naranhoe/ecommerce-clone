a38f6edcbcc07efb8db99c3101fb54f5068a93c4
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-13 00:51:01 -0500

    Add categories menu

--- admin/categories.php
+++ admin/categories.php
@@ -2,6 +2,63 @@
   require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
   include 'includes/head.php';
   include 'includes/navigation.php';
+
+  $sql = "SELECT * FROM categories WHERE parent = 0";
+  $result = $db->query($sql);
 ?>
+<h2 class="text-center">Categories</h2><hr>
+<div class="row">
+
+  <!-- Form -->
+  <div></div>
+    <form class="form" action="categories.php" method="post">
+      <div class="form-group col-md-6">
+        <label for="parent">Parent</label>
+        <select class="form-control" name="parent" id='parent'>
+          <option value="0">Parent</option>
+          <?php while($parent = mysqli_fetch_assoc($result)): ?>
+            <option value="<?php echo($parent['id']); ?>"><?php echo($parent['category']); ?></option>
+          <?php endwhile; ?>
+        </select>
+      </div>
+    </form>
+  <!-- Category Table -->
+    <div class="col-md-6">
+      <table class='table table-bordered'>
+        <thead><br>
+          <th>Categories</th><th>Parent</th><th></th>
+        </thead>
+        <tbody>
+          <?php
+            $sql = "SELECT * FROM categories WHERE parent = 0";
+            $result = $db->query($sql);
+            while($parent = mysqli_fetch_assoc($result)):
+              $parent_id = (int)$parent['id'];
+              $sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
+              $child_result = $db->query($sql2);
+          ?>
+            <tr class="bg-primary">
+              <td><?php echo($parent['category']); ?></td>
+              <td>Parent</td>
+              <td>
+                <a href="categories.php?edit=<?php echo($parent['id']); ?>" class="btn btn-xs btn-default"><span class='glyphicon glyphicon-pencil'></span></a>
+                <a href="categories.php?delete=<?php echo($parent['id']); ?>" class="btn btn-xs btn-default"><span class='glyphicon glyphicon-remove-sign'></span></a>
+              </td>
+            </tr>
+            <?php while($child = mysqli_fetch_assoc($child_result)): ?>
+              <tr class="bg-info">
+                <td><?php echo($child['category']); ?></td>
+                <td><?php echo($parent['category']); ?></td>
+                <td>
+                  <a href="categories.php?edit=<?php echo($child['id']); ?>" class="btn btn-xs btn-default"><span class='glyphicon glyphicon-pencil'></span></a>
+                  <a href="categories.php?delete=<?php echo($child['id']); ?>" class="btn btn-xs btn-default"><span class='glyphicon glyphicon-remove-sign'></span></a>
+                </td>
+              </tr>
+            <?php endwhile; ?>
+          <?php endwhile; ?>
+        </tbody>
+      </table>
+    </div>
+</div>
 
 <?php include 'includes/footer.php'; ?>
