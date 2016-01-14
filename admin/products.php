8b866e5c90b0a01285116eaa0b19438527d77542
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-14 17:15:12 -0500

    Products Price and Feature render correctly from the database

--- admin/products.php
+++ admin/products.php
@@ -2,8 +2,11 @@
   require_once $_SERVER['DOCUMENT_ROOT'] . "/core/init.php";
   include "includes/head.php";
   include "includes/navigation.php";
+
   $sql = "SELECT * FROM products WHERE deleted = 0";
   $presults = $db->query($sql);
+
+  // $sql2 = "SELECT * FROM categories WHERE category = ''";
  ?>
 
  <h2 class="text-center">Products</h2><hr>
@@ -17,9 +20,9 @@
            <a href="products.php?delete=<?php echo $product['id']; ?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
          </td>
          <td><?php echo $product['title']; ?></td>
+         <td><?php echo money($product['price']); ?></td>
          <td></td>
-         <td></td>
-         <td></td>
+         <td><a href="products.php?featured=<?php echo (($product['featured'] == 0)?'1':'0'); ?>&id=<?php echo $product['id']; ?>" class="btn btn-xs btn-default "><span class="glyphicon glyphicon-<?php echo (($product['featured'] == 1)?'minus':'plus') ?>"></span></a></td>
          <td></td>
        </tr>
      <?php endwhile; ?>
