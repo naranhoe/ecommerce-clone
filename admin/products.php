2c0ca6da958ebb8374fe3fd2ffdb4783dc8f8503
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-14 16:25:27 -0500

    Add Products on nav bar

---
+++ admin/products.php
@@ -0,0 +1,7 @@
+<?php
+  require_once $_SERVER['DOCUMENT_ROOT'] . "/core/init.php";
+  include "includes/head.php";
+  include "includes/navigation.php";
+ ?>
+
+ <?php include "includes/footer.php"; ?>
