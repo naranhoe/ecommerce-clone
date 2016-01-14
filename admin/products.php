c50ae81e7b4ef4e557e6e796942f70e56d53b5db
Jorge Naranjo <j.naranjojr@gmail.com>
2016-01-14 16:44:20 -0500

    Add table to products page

--- admin/products.php
+++ admin/products.php
@@ -2,6 +2,27 @@
   require_once $_SERVER['DOCUMENT_ROOT'] . "/core/init.php";
   include "includes/head.php";
   include "includes/navigation.php";
+  $sql = "SELECT * FROM products WHERE deleted = 0";
+  $presults = $db->query($sql);
  ?>
+
+ <h2 class="text-center">Products</h2><hr>
+ <table class="table table-bordered table-condensed table-striped">
+   <thead><th></th><th>Products</th><th>Price</th><th>Category</th><th>Feature</th><th>Sold</th></thead>
+   <tbody>
+     <?php while($product = mysqli_fetch_assoc($presults)): ?>
+       <tr>
+         <td>
+           <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+         </td>
+         <td></td>
+         <td></td>
+         <td></td>
+         <td></td>
+         <td></td>
+       </tr>
+     <?php endwhile; ?>
+   </tbody>
+ </table>
 
  <?php include "includes/footer.php"; ?>
